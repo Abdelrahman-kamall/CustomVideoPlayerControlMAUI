f27b09ac0c27f15ba4ff3a6f3d1e8400e8a5d36d
Abdelrahman Kamal Nasr <aknasr@ejada.com>
2023-07-17 21:49:29 +0200

    Add project files.

---
+++ CustomControlPOC/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomControlPOC;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CustomControlPOC/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CustomControlPOC;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
